5cb8c4d72462ac3a26fe32ba3364a7a4aa2074da
Jesse Shipley <jships@Jesses-MacBook-Pro.local>
2025-01-06 15:50:08 -0600

    basic sign in/out and spec

---
+++ spec/teachers/teacher_authentication_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.feature "Teacher Authentication", type: :feature do
+  scenario "Teacher can sign up, sign in, and sign out" do
+    # Teacher signs up
+    visit new_teacher_registration_path
+    fill_in "Email", with: "teacher@example.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+
+    # Verify that teacher is signed in by checking for the Logout link
+    expect(page).to have_link("Logout")
+
+    # Teacher signs out
+    click_link "Logout"
+    save_and_open_page
+
+    # Verify that teacher is signed out
+    expect(page).to have_content("Signed out successfully.")
+  end
+end
