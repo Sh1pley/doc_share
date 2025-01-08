5f41ad88d64c6a66a127de6025d2bd2c78c83251
Jesse Shipley <jships@Jesses-MacBook-Pro.local>
2025-01-08 09:39:07 -0600

    implement RedCarpet for rendering, fix spec

--- app/models/markdown_document.rb
+++ app/models/markdown_document.rb
@@ -2,9 +2,13 @@ class MarkdownDocument < Document
   validate :markdown_file_type
 
   def render_html
+    return "" unless file&.download
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, tables: true)
+    markdown.render(file.download)
   end
 
   private
+
   def markdown_file_type
     errors.add(:file, "must be a Markdown file.") unless file.content_type == "text/markdown"
   end
--- spec/models/markdown_document_spec.rb
+++ spec/models/markdown_document_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe MarkdownDocument do
   describe "#render_html" do
     it "implements render method" do
-      document = FactoryBot.build(:markdown_document)
+      document = FactoryBot.create(:markdown_document)
 
       expect { document.render_html }.not_to raise_error
     end
